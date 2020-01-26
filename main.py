a1d09e5d6cb08bea83e175150d737455554f2295
wangke <wangke_0809@126.com>
2020-01-26 16:32:32 +0800

    fix action

--- main.py
+++ main.py
@@ -5,23 +5,19 @@ from spider import Spider
 import time
 from state import State
 import traceback
+import sys
 
 log = Logger.getLogger("nCoV", config.LoggerJsonConfig)
 
-if __name__ == '__main__':
-    token = config.PushToken
-    keyword = config.PushKeyWord
-    weiboScf = config.WeiboSCFUrl
-    redisUrl = config.Redis
-    push = Push(token=token, keyWord=keyword, weiboSCF=weiboScf)
-    spider = Spider()
-    state = State(redisUrl)
-    spider.postId = state.getPostId()
+
+def run():
     try:
         msgs = spider.getPosts()
         log.info("get %d posts", len(msgs))
+        i = 0
         if len(msgs) > 0:
             for id, tag, city, title, text, url in msgs:
+                i += 1
                 pushTitle = ""
                 if tag:
                     pushTitle = "#" + tag + "#"
@@ -29,8 +25,39 @@ if __name__ == '__main__':
                     pushTitle += "#" + city + "#"
                 pushTitle += "【" + title + "】"
                 pushText = "#疫情聚合#" + pushTitle + "\r\n" + text + " 【转自：" + url + ' 】'
+                log.info("send msg")
                 push.sendMsg(pushTitle, pushText)
-                state.setPostId(spider.postId)
-                time.sleep(60)
+                if usrAction:
+                    state.setPostId(spider.postId)
+                if len(msgs) != i:
+                    log.info("sleep 20s")
+                    time.sleep(20)
     except Exception as e:
         log.error("error: %s", traceback.format_exc())
+
+
+if __name__ == '__main__':
+    # use github action to run?
+    if len(sys.argv) > 1:
+        usrAction = True
+        log.info("use github action")
+    else:
+        usrAction = False
+
+    push = Push(token=config.PushToken, keyWord=config.PushKeyWord, weiboSCF=config.WeiboSCFUrl,
+                weiboRef=config.WeiboRef, weiboCookie=config.WeiboCookie)
+    spider = Spider()
+
+    if usrAction:
+        state = State(config.Redis)
+        spider.postId = state.getPostId()
+    else:
+        spider.postId = 0
+
+    while True:
+        run()
+        if usrAction:
+            break
+        else:
+            log.info("sleep 60s")
+            time.sleep(60)
